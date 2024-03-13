4375ff59ff903c3197b0b64dc28e548fcda29996
Antonio Madrid C <137960018+AntonioMC-Toni@users.noreply.github.com>
2024-03-12 21:36:11 -0300

    Initial Commit

---
+++ iTunesTop10/iTunesTop10App.swift
@@ -0,0 +1,17 @@
+//
+//  iTunesTop10App.swift
+//  iTunesTop10
+//
+//  Created by Antonio Madrid on 12-03-24.
+//
+
+import SwiftUI
+
+@main
+struct iTunesTop10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
